ed693b762141e18c2d5d1b3c32eaa2256a5cacd6
maciejs11 <maciejs1112@gmail.com>
2020-01-04 17:41:10 +0100

    stworzenie szkieletu userprofile repository

---
+++ Inzynierka/Inzynierka/Models/UserProfiles/IUserProfileRepository.cs
@@ -0,0 +1,13 @@
+﻿using Inzynierka.Models.ApplicationUsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inzynierka.Models.UserProfiles
+{
+    public interface IUserProfileRepository
+    {
+        ApplicationUser GetUserProfile();
+    }
+}
---
+++ Inzynierka/Inzynierka/Models/UserProfiles/UserProfileRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Inzynierka.Data;
+using Inzynierka.Models.ApplicationUsers;
+
+namespace Inzynierka.Models.UserProfiles
+{
+    public class UserProfileRepository : IUserProfileRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UserProfileRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public ApplicationUser GetUserProfile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
